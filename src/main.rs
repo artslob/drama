ebd285f6e14518fb7df8f9cdc150fb549389158d
artslob <artslob@yandex.ru>
2021-08-04 23:32:42 +0300

    parse yaml config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Deserialize;
+
+#[derive(Debug, PartialEq, Deserialize)]
+struct Config {
+    client_id: String,
+    client_secret: String,
+    user_agent: String,
+}
+
+type Error = Box<dyn std::error::Error + Sync + Send>;
+type Result<T> = std::result::Result<T, Error>;
+
+fn main() -> crate::Result<()> {
+    // TODO accept config path as env variable
+    let config_path = "configs/drama-config.yml";
+    let config_file = std::fs::File::open(config_path)?;
+    let config: Config = serde_yaml::from_reader(config_file)?;
+
+    println!("{:#?}", config);
+
+    Ok(())
 }
