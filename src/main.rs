ace4a493ce39f5d8f938ea8f4d3d1fade9479e36
artslob <artslob@yandex.ru>
2021-08-05 23:28:01 +0300

    display user agent as string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter};
+
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
@@ -15,6 +17,28 @@ struct UserAgent {
     reddit_username: String,
 }
 
+impl Display for UserAgent {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let os = std::env::consts::OS;
+        let version = env!("CARGO_PKG_VERSION");
+        let repository = env!("CARGO_PKG_REPOSITORY");
+        let app_id = repository.split("://").last().unwrap_or_default();
+        let app_id = if app_id.is_empty() {
+            env!("CARGO_PKG_NAME")
+        } else {
+            app_id
+        };
+        write!(
+            f,
+            "{}:{}:{} (by /u/{})",
+            self.platform.as_ref().unwrap_or(&os.into()),
+            self.app_id.as_ref().unwrap_or(&app_id.into()),
+            self.version.as_ref().unwrap_or(&version.into()),
+            self.reddit_username
+        )
+    }
+}
+
 type Error = Box<dyn std::error::Error + Sync + Send>;
 type Result<T> = std::result::Result<T, Error>;
 
@@ -23,26 +47,9 @@ fn main() -> crate::Result<()> {
     let config_path = "configs/drama-config.yml";
     let config_file = std::fs::File::open(config_path)?;
     let config: Config = serde_yaml::from_reader(config_file)?;
-    let os = std::env::consts::OS;
-    let version = env!("CARGO_PKG_VERSION");
-    let repository = env!("CARGO_PKG_REPOSITORY");
-    let app_id = repository.split("://").last().unwrap_or_default();
-    let app_id = if app_id.is_empty() {
-        env!("CARGO_PKG_NAME")
-    } else {
-        app_id
-    };
-    let user_agent = &config.user_agent;
-    let user_agent = format!(
-        "{}:{}:{} (by /u/{})",
-        user_agent.platform.as_ref().unwrap_or(&os.into()),
-        user_agent.app_id.as_ref().unwrap_or(&app_id.into()),
-        user_agent.version.as_ref().unwrap_or(&version.into()),
-        user_agent.reddit_username,
-    );
 
     println!("{:#?}", config);
-    println!("{}", user_agent);
+    println!("{}", config.user_agent);
 
     Ok(())
 }
