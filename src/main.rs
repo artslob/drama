1cc2b5d55d24e0b5d04b076e94ed6e488527c7b1
artslob <artslob@yandex.ru>
2021-08-08 12:10:54 +0300

    parse top posts to structs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
+use serde::{Deserialize, Serialize};
+
 mod config;
 mod reddit;
 
 type Error = Box<dyn std::error::Error + Sync + Send>;
 type Result<T> = std::result::Result<T, Error>;
 
-#[derive(serde::Deserialize, Debug)]
-struct Token {
-    access_token: String,
-    token_type: String,
-    expires_in: i32,
-    scope: String,
-}
-
-#[derive(serde::Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Subreddit {
     display_name: String,
     header_title: String,
@@ -25,7 +19,56 @@ struct Subreddit {
     url: String,
 }
 
-#[derive(serde::Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
+struct Post {
+    archived: bool,
+    author: String,
+    author_fullname: String,
+    author_is_blocked: bool,
+    author_premium: bool,
+    created_utc: f32,
+    downs: i32,
+    edited: bool,
+    gilded: i32,
+    hidden: bool,
+    hide_score: bool,
+    id: String,
+    is_created_from_ads_ui: bool,
+    is_crosspostable: bool,
+    is_meta: bool,
+    is_original_content: bool,
+    is_reddit_media_domain: bool,
+    is_robot_indexable: bool,
+    is_self: bool,
+    is_video: bool,
+    locked: bool,
+    name: String,
+    num_comments: i32,
+    num_crossposts: i32,
+    over_18: bool,
+    pinned: bool,
+    quarantine: bool,
+    saved: bool,
+    score: i32,
+    subreddit_subscribers: i32,
+    title: String,
+    total_awards_received: i32,
+    ups: i32,
+    upvote_ratio: f32,
+    url: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Listing<T> {
+    after: Option<String>,
+    before: Option<String>,
+    children: Vec<T>,
+    dist: i32,
+    geo_filter: String,
+    modhash: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 struct Data<T> {
     data: T,
     kind: String,
@@ -44,6 +87,8 @@ fn main() -> crate::Result<()> {
 
     let tail = format!("user/{}/about", config.user_agent.reddit_username);
     let json: serde_json::Value = client.get(&tail)?.json()?;
+
+    let json: Data<Listing<Data<Post>>> = client.get("top")?.json()?;
     println!("{}", serde_json::to_string_pretty(&json)?);
 
     Ok(())
