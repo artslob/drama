9a4ac14a84cd1556ec4f5efdd6bf82cb9d47658b
artslob <artslob@yandex.ru>
2021-08-28 21:06:48 +0300

    publish and consume

---
+++ src/bin/consume.rs
@@ -0,0 +1,55 @@
+use futures_util::StreamExt;
+use lapin::{options::*, types::FieldTable, Connection, ConnectionProperties, Result};
+use log::info;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "info");
+    }
+    env_logger::init();
+
+    let addr = std::env::var("AMQP_ADDR").unwrap_or_else(|_| "amqp://127.0.0.1:5672/%2f".into());
+
+    let conn = Connection::connect(
+        &addr,
+        ConnectionProperties::default().with_default_executor(8),
+    )
+    .await?;
+
+    let channel = conn.create_channel().await?;
+
+    let queue = channel
+        .queue_declare(
+            "hello",
+            QueueDeclareOptions::default(),
+            FieldTable::default(),
+        )
+        .await?;
+
+    info!("Declared queue {:?}", queue);
+
+    let mut consumer = channel
+        .basic_consume(
+            "hello",
+            "my_consumer",
+            BasicConsumeOptions::default(),
+            FieldTable::default(),
+        )
+        .await?;
+
+    info!("will consume");
+    while let Some(delivery) = consumer.next().await {
+        let (_, delivery) = delivery.expect("error in consumer");
+        match std::str::from_utf8(&delivery.data) {
+            Ok(s) => {
+                info!("got string {}", s)
+            }
+            Err(e) => {
+                info!("error! {}", e)
+            }
+        }
+        delivery.ack(BasicAckOptions::default()).await.expect("ack");
+    }
+    Ok(())
+}
---
+++ src/bin/publish.rs
@@ -0,0 +1,53 @@
+use std::time::Duration;
+
+use lapin::{
+    options::*, publisher_confirm::Confirmation, types::FieldTable, BasicProperties, Connection,
+    ConnectionProperties, Result,
+};
+use log::info;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "info");
+    }
+    env_logger::init();
+
+    let addr = std::env::var("AMQP_ADDR").unwrap_or_else(|_| "amqp://127.0.0.1:5672/%2f".into());
+
+    let conn = Connection::connect(
+        &addr,
+        ConnectionProperties::default().with_default_executor(8),
+    )
+    .await?;
+
+    let channel = conn.create_channel().await?;
+
+    let queue = channel
+        .queue_declare(
+            "hello",
+            QueueDeclareOptions::default(),
+            FieldTable::default(),
+        )
+        .await?;
+
+    info!("Declared queue {:?}", queue);
+
+    loop {
+        let now = chrono::Local::now().to_string();
+        let payload = format!("message constructed at {}", now);
+        let confirm = channel
+            .basic_publish(
+                "",
+                "hello",
+                BasicPublishOptions::default(),
+                payload.clone().into_bytes(),
+                BasicProperties::default(),
+            )
+            .await?
+            .await?;
+        assert_eq!(confirm, Confirmation::NotRequested);
+        tokio::time::sleep(Duration::from_secs(1)).await;
+        println!("sent {}", payload);
+    }
+}
