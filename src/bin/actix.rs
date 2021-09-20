50ef3b03dd755d5527fded09e36b87970f14eee0
artslob <artslob@yandex.ru>
2021-09-20 21:57:06 +0300

    parse code in callback

--- src/bin/actix.rs
+++ src/bin/actix.rs
@@ -3,7 +3,7 @@ use actix_web as aw;
 #[aw::get("/start")]
 async fn start(config: aw::web::Data<drama::config::Config>) -> aw::HttpResponse {
     let scope = "identity,edit,history,mysubreddits,read";
-    let redirect_uri = "http://127.0.0.1:9999/";
+    let redirect_uri = "http://127.0.0.1:9999/callback";
     let state = "64990aeb-5178-43d3-8ccb-110962843622";
     let url = format!(
         "https://www.reddit.com/api/v1/authorize?\
@@ -23,6 +23,18 @@ async fn start(config: aw::web::Data<drama::config::Config>) -> aw::HttpResponse
         .body(link)
 }
 
+#[derive(Debug, serde::Deserialize)]
+struct CallbackParams {
+    code: String,
+    state: String,
+}
+
+#[aw::get("/callback")]
+async fn callback(params: aw::web::Query<CallbackParams>) -> impl aw::Responder {
+    println!("code {}\nstate {}", params.code, params.state);
+    "nice"
+}
+
 #[actix_web::main]
 async fn main() -> drama::Result<()> {
     let config = drama::config::Config::from_env()?;
@@ -30,6 +42,7 @@ async fn main() -> drama::Result<()> {
         aw::App::new()
             .app_data(aw::web::Data::new(config.clone()))
             .service(start)
+            .service(callback)
     };
     aw::HttpServer::new(factory)
         .bind("127.0.0.1:9999")?
