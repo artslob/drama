468f2076e1eb87431bae8c71a56ab46962d015c9
artslob <artslob@yandex.ru>
2021-09-25 22:01:39 +0300

    simple sqlx usage

---
+++ src/bin/sqlx.rs
@@ -0,0 +1,20 @@
+use sqlx::postgres::PgPoolOptions;
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect_timeout(Duration::from_secs(5))
+        .connect("postgres://drama_user:drama_pass@localhost:5932/drama_db")
+        .await?;
+
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&pool)
+        .await?;
+
+    assert_eq!(row.0, 150);
+
+    Ok(())
+}
