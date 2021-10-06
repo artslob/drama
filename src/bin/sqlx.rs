69cbb555aa3465297803e8c3175df9d72bd97006
artslob <artslob@yandex.ru>
2021-10-06 21:59:05 +0300

    insert token

--- src/bin/sqlx.rs
+++ src/bin/sqlx.rs
@@ -1,6 +1,15 @@
 use sqlx::postgres::PgPoolOptions;
 use std::time::Duration;
 
+#[derive(serde::Deserialize, sqlx::FromRow, Debug)]
+struct Token {
+    access_token: String,
+    refresh_token: String,
+    token_type: String,
+    expires_in: i32,
+    scope: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
     let pool = PgPoolOptions::new()
@@ -13,6 +22,22 @@ async fn main() -> Result<(), sqlx::Error> {
         .bind(150_i64)
         .fetch_one(&pool)
         .await?;
+
+    let token: Token = sqlx::query_as::<_, Token>(
+        "INSERT INTO token (uuid, access_token, refresh_token, token_type, \
+    expires_in, scope) VALUES ($1, $2, $3, $4, $5, $6)  \
+    RETURNING access_token, refresh_token, token_type, expires_in, scope",
+    )
+    .bind(uuid::Uuid::new_v4())
+    .bind("access")
+    .bind("refresh")
+    .bind("token type")
+    .bind(1i32)
+    .bind("scope")
+    .fetch_one(&pool)
+    .await?;
+
+    println!("{:#?}", token);
 
     assert_eq!(row.0, 150);
 
