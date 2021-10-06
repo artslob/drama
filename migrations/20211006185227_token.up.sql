24820724fdeee4655c0988a22c97f5de547a0909
artslob <artslob@yandex.ru>
2021-10-06 21:59:36 +0300

    add migration for token

---
+++ migrations/20211006185227_token.up.sql
@@ -0,0 +1,10 @@
+-- Add up migration script here
+CREATE TABLE IF NOT EXISTS token
+(
+    uuid          uuid primary key,
+    access_token  varchar not null,
+    refresh_token varchar not null,
+    token_type    varchar not null,
+    expires_in    int     not null,
+    scope         varchar not null
+);
