d33a2c5c1e5d2df977734aa29fc3b9d2b2818d0b
artslob <artslob@yandex.ru>
2021-10-31 18:22:12 +0300

    add user table

---
+++ migrations/20211031150651_user.up.sql
@@ -0,0 +1,22 @@
+-- Add up migration script here
+CREATE TABLE IF NOT EXISTS "user"
+(
+    -- TODO transform id from base36 to base10?
+    id                 varchar primary key,
+    accept_followers   boolean not null,
+    has_subscribed     boolean not null,
+    has_verified_email boolean not null,
+    hide_from_robots   boolean not null,
+    is_blocked         boolean not null,
+    is_employee        boolean not null,
+    is_friend          boolean not null,
+    is_gold            boolean not null,
+    is_mod             boolean not null,
+    name               varchar not null,
+    total_karma        int     not null,
+    link_karma         int     not null,
+    awardee_karma      int     not null,
+    awarder_karma      int     not null,
+    comment_karma      int     not null,
+    verified           boolean not null
+);
