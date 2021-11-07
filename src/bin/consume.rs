1f2e2c505f1e98e0586b36ae73127f8d0f09c828
artslob <artslob@yandex.ru>
2021-11-07 17:32:10 +0300

    implement personal task to save user
    1. request data about user
    2. save user to db
    3. save access and refresh tokens to separate tables
    4. delete tokens from registration table

--- src/bin/consume.rs
+++ src/bin/consume.rs
@@ -1,3 +1,6 @@
+use drama::config::Config;
+use drama::reddit::model::User;
+use drama::task::{Task, TaskCommon};
 use futures::TryStreamExt;
 use futures_util::StreamExt;
 use lapin::{
@@ -7,8 +10,6 @@ use log::info;
 use sqlx::Row;
 use std::time::Duration;
 use uuid::Uuid;
-
-use drama::task::Task;
 
 #[tokio::main]
 async fn main() -> drama::Result<()> {
@@ -24,6 +25,9 @@ async fn main() -> drama::Result<()> {
         ConnectionProperties::default().with_default_executor(8),
     )
     .await?;
+
+    let config = drama::config::Config::from_env()?;
+    let config: &'static Config = Box::leak(Box::new(config));
 
     let pool = sqlx::postgres::PgPoolOptions::new()
         .max_connections(5)
@@ -63,24 +67,118 @@ async fn main() -> drama::Result<()> {
             Ok(task) => task,
             Err(_) => continue,
         };
-        tokio::spawn(handle_task(channel.clone(), task, pool.clone()));
+        tokio::spawn(handle_task(config, channel.clone(), task, pool.clone()));
         delivery.ack(BasicAckOptions::default()).await.expect("ack");
     }
     Ok(())
 }
 
-async fn handle_task(channel: Channel, task: Task, pool: sqlx::PgPool) -> drama::Result<()> {
+async fn handle_task(
+    config: &'static Config,
+    channel: Channel,
+    task: Task,
+    pool: sqlx::PgPool,
+) -> drama::Result<()> {
     info!("msg waited");
-    match task {
-        Task::CreateUser { common, uid } => {
-            info!(
-                "got task to create user created at {} with row uuid {}",
-                common.created_at, uid
-            );
-        }
-        Task::CreateUserCron(_) => create_user_cron(channel, &pool).await?,
-        _ => {}
+    let result = match task {
+        Task::CreateUser { common, uid } => create_user(config, &pool, common, uid).await,
+        Task::CreateUserCron(_) => create_user_cron(channel, &pool).await,
+        _ => return Ok(()),
     };
+    match result {
+        Ok(_) => info!("task handled successfully"),
+        Err(err) => info!("result of handled task: {}", err),
+    }
+    Ok(())
+}
+
+async fn create_user(
+    config: &'static Config,
+    pool: &sqlx::PgPool,
+    common: TaskCommon,
+    uid: Uuid,
+) -> drama::Result<()> {
+    info!(
+        "got task to create user created at {} with row uuid {}",
+        common.created_at, uid
+    );
+    let token =
+        sqlx::query_as::<_, RegistrationToken>("SELECT * FROM registration_token WHERE uuid = $1")
+            .bind(&uid)
+            .fetch_optional(pool)
+            .await?;
+    let token = match token {
+        Some(token) => token,
+        None => {
+            info!("not found registration token with uid {}", uid);
+            return Ok(());
+        }
+    };
+    let user: User = reqwest::Client::builder()
+        .user_agent(config.user_agent.to_string())
+        .build()?
+        .get("https://oauth.reddit.com/api/v1/me")
+        .bearer_auth(&token.access_token)
+        .send()
+        .await?
+        .error_for_status()?
+        .json()
+        .await?;
+    info!("got user with id {}", &user.id);
+    let mut tx = pool.begin().await?;
+    sqlx::query(
+        r#"INSERT INTO "user" (id, accept_followers, has_subscribed, has_verified_email,
+        hide_from_robots, is_employee, is_gold, is_mod, name,
+        total_karma, link_karma, awardee_karma, awarder_karma, comment_karma, verified)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
+        ON CONFLICT (id) DO NOTHING"#,
+    )
+    .bind(&user.id)
+    .bind(user.accept_followers)
+    .bind(user.has_subscribed)
+    .bind(user.has_verified_email)
+    .bind(user.hide_from_robots)
+    .bind(user.is_employee)
+    .bind(user.is_gold)
+    .bind(user.is_mod)
+    .bind(user.name)
+    .bind(user.total_karma)
+    .bind(user.link_karma)
+    .bind(user.awardee_karma)
+    .bind(user.awarder_karma)
+    .bind(user.comment_karma)
+    .bind(user.verified)
+    .execute(&mut tx)
+    .await?;
+    sqlx::query(r#"DELETE FROM registration_token WHERE uuid = $1"#)
+        .bind(&uid)
+        .execute(&mut tx)
+        .await?;
+    sqlx::query(
+        r"INSERT INTO refresh_token (uuid, user_id, refresh_token, token_type, scope)
+          VALUES ($1, $2, $3, $4, $5)
+          ON CONFLICT ON CONSTRAINT uq_refresh_token_refresh_token DO NOTHING",
+    )
+    .bind(Uuid::new_v4())
+    .bind(&user.id)
+    .bind(&token.refresh_token)
+    .bind(&token.token_type)
+    .bind(&token.scope)
+    .execute(&mut tx)
+    .await?;
+    sqlx::query(
+        r"INSERT INTO access_token (uuid, user_id, access_token, token_type, expires_in, scope)
+          VALUES ($1, $2, $3, $4, $5, $6)",
+    )
+    .bind(Uuid::new_v4())
+    .bind(&user.id)
+    .bind(&token.access_token)
+    .bind(&token.token_type)
+    .bind(&token.expires_in)
+    .bind(&token.scope)
+    .execute(&mut tx)
+    .await?;
+    tx.commit().await?;
     Ok(())
 }
 
@@ -95,8 +193,7 @@ struct RegistrationToken {
 }
 
 async fn create_user_cron(channel: Channel, pool: &sqlx::PgPool) -> drama::Result<()> {
-    // TODO select tokens and send them as personal tasks
-    info!("got cron task to create user... sending new task");
+    info!("got cron task to create user... sending new tasks");
 
     let mut uuids = sqlx::query("SELECT uuid FROM registration_token LIMIT 10").fetch(pool);
 
