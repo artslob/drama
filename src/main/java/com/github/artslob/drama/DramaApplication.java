3f3e01551878487ef57adb370642a346f896208d
artslob <artslob@yandex.ru>
2022-11-26 15:09:42 +0700

    add spring boot project
    generated by https://start.spring.io/

---
+++ src/main/java/com/github/artslob/drama/DramaApplication.java
@@ -0,0 +1,13 @@
+package com.github.artslob.drama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DramaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DramaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/artslob/drama/DramaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.artslob.drama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DramaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
