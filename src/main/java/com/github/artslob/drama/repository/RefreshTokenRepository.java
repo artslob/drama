0043636cd210fd9d958da82e0a10d1a655de1dce
artslob <artslob@yandex.ru>
2022-12-05 16:59:22 +0700

    add repo

---
+++ src/main/java/com/github/artslob/drama/repository/RefreshTokenRepository.java
@@ -0,0 +1,7 @@
+package com.github.artslob.drama.repository;
+
+import com.github.artslob.drama.entity.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
+}
