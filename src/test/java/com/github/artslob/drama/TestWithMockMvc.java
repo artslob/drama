ae35ac564723467d8a77401c06817f09eb65f44b
artem_bigpay <artem.s@bigpayme.com>
2022-12-11 22:34:37 +0700

    add test with mock mvc

---
+++ src/test/java/com/github/artslob/drama/TestWithMockMvc.java
@@ -0,0 +1,24 @@
+package com.github.artslob.drama;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TestWithMockMvc {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void checkRoot() throws Exception {
+        mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(content().string(containsString("client_id=")));
+    }
+}
