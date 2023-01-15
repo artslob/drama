3bc24e4049f8e5c5d919292f7d1b265328d3768a
artslob <artslob@yandex.ru>
2023-01-15 14:16:58 +0700

    add "quick and dirty" nix shell script

---
+++ dev.nix
@@ -0,0 +1,3 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/4d2b37a84fad1091b9de401eb450aae66f1a741e.tar.gz
+#! nix-shell --pure -p jdk -p docker-compose
