8433f23ee06e5cc0ad4b455fd5d220c771880405
artslob <artslob@yandex.ru>
2023-01-16 21:05:08 +0700

    add nixfmt to pre-commit

--- dev.nix
+++ dev.nix
@@ -1,14 +1,17 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4d2b37a84fad1091b9de401eb450aae66f1a741e.tar.gz") {} }:
+{ pkgs ? import (fetchTarball
+  "https://github.com/NixOS/nixpkgs/archive/4d2b37a84fad1091b9de401eb450aae66f1a741e.tar.gz")
+  { } }:
 
 pkgs.mkShell {
-  buildInputs = [
-    pkgs.git
-    pkgs.pre-commit
-    pkgs.nixfmt
-    pkgs.which
-    pkgs.htop
-    pkgs.jdk
-    pkgs.docker-compose
-    pkgs.nodejs # required for spotless:prettier
+  buildInputs = with pkgs; [
+    git
+    vim
+    pre-commit
+    nixfmt
+    which
+    htop
+    jdk
+    docker-compose
+    nodejs # required for spotless:prettier
   ];
 }
